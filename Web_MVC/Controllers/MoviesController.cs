bdfd8ed25c815b744899ab566330724bc494f8f9
Administrator <Administrator@PC201809011208>
2019-05-21 23:32:30 +0800

    添加项目文件。

---
+++ Web_MVC/Controllers/MoviesController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Web_MVC.Models;
+
+namespace Web_MVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private MovieDBContext db = new MovieDBContext();
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            Server.HtmlEncode("");//防止SQL攻击
+            Session["a"] = "aa";//创建
+            Session.Timeout = 30;//设置30分钟（可设置单个session的过期时间）
+
+
+            return View(db.Movie.ToList());
+        }
+
+        // GET: Movies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                //id为空时，返回错误码（如：404）
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movie.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]//防止跨域攻击
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movie.Add(movie);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+               //返回http的状态码（如：404）
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movie.Find(id);//查找id
+            if (movie == null)
+            {
+                return HttpNotFound();//
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        {//Include绑定需要修改的字段写在里面
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(movie).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movie.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Movie movie = db.Movie.Find(id);
+            db.Movie.Remove(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Web_MVC/Models/Movie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+//using Microsoft.EntityFrameworkCore;
+
+
+
+namespace Web_MVC.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [Display(Name ="标题")]
+        public string Title { get; set; }
+
+        [Display(Name ="日期")]//自定义属性显示的名称
+        [DataType(DataType.Date)]//只显示日期
+        [DisplayFormat(DataFormatString ="{0:yyyy/MM/dd}",ApplyFormatInEditMode =true)]//显示的格式
+        public DateTime ReleaseDate { get; set; }
+        
+        [Display(Name ="类型")]
+        public string Genre { get; set; }
+
+        [Display(Name ="价格")]
+        public decimal Price { get; set; }
+    }
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movie { get; set; }
+        //protected override void OnConfiguring(DbContextOptionsBuilder options)
+        //{
+
+        //}
+    }
+
+     
+}
